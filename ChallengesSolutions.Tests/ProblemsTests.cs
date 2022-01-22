0342514290d72f89c731145a14213c0b64886e58
Richard Almonte <richard_almonte@hotmail.com>
2022-01-23 00:41:41 +0100

    - Add HackerRank "Diagonal Difference" problem with 2 solutions

--- ChallengesSolutions.Tests/ProblemsTests.cs
+++ ChallengesSolutions.Tests/ProblemsTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ChallengesSolutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ChallengesSolutions
@@ -17,9 +19,30 @@ namespace ChallengesSolutions
         [DataRow(new int[] { }, 0)]
         [DataRow(null, 0)]
         [Timeout(350)]
-        public void BirthdayCakeCandles_Test(int[] candles, int expected)
+        public void BirthdayCakeCandles_Test(int[] input, int expected)
         {
-            var result = Problems.BirthdayCakeCandles(candles?.ToList());
+            var result = Problems.BirthdayCakeCandles(input?.ToList());
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod()]
+        [DataRow(new object[] { new int[] { -1, 1, -7, -8 }, new int[] { -10, -8, -5, -2 }, new int[] { 0, 9, 7, -1 }, new int[] { 4, 4, -2, 1 } }, 1)]
+        [DataRow(new object[] { new int[] { 11, 2, 4 }, new int[] { 4, 5, 6 }, new int[] { 10, 8, -12 } }, 15)]
+        public void DiagonalDifference_Bitwise_Test(object[] input, int expected)
+        {
+            var result = Problems.DiagonalDifference_Bitwise(input.Select(x => ((int[])x).ToList()).ToList());
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow(new object[] { new int[] { -1, 1, -7, -8 }, new int[] { -10, -8, -5, -2 }, new int[] { 0, 9, 7, -1 }, new int[] { 4, 4, -2, 1 } }, 1)]
+        [DataRow(new object[] { new int[] { 11, 2, 4 }, new int[] { 4, 5, 6 }, new int[] { 10, 8, -12 } }, 15)]
+        public void DiagonalDifference_Test(object[] input, int expected)
+        {
+            var result = Problems.DiagonalDifference_Accepted(input.Select(x => ((int[])x).ToList()).ToList());
+
             Assert.AreEqual(expected, result);
         }
     }
--- ChallengesSolutions/Problems.cs
+++ ChallengesSolutions/Problems.cs
@@ -22,5 +22,55 @@
             var max = candles.Max();
             return candles.Count(c => c >= max);
         }
+
+
+        /// <summary>
+        /// HackerRank "Diagonal Difference" problem
+        /// Solution using Bitwise Exclusive OR (XOR). Not working on HackerRank
+        /// found at https://www.hackerrank.com/challenges/diagonal-difference
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int DiagonalDifference_Bitwise(List<List<int>> arr)
+        {
+
+            int diagA = arr[0][0];
+            int diagB = arr[0][^1];
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                List<int> row = arr[i];
+
+                diagA += row[i];
+                diagB += row[^(i + 1)];
+
+            }
+
+            return Math.Abs(diagA - diagB);
+        }
+
+        /// <summary>
+        /// HackerRank "Diagonal Difference" problem
+        /// Solution accepted on HackerRank
+        /// found at https://www.hackerrank.com/challenges/diagonal-difference
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int DiagonalDifference_Accepted(List<List<int>> arr)
+        {
+            int diagA = arr[0][0];
+            int diagB = arr[0][arr[0].Count - 1];
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                List<int> row = arr[i];
+
+                diagA += row[i];
+                diagB += row[row.Count - (i + 1)];
+
+            }
+
+            return Math.Abs(diagA - diagB);
+        }
     }
 }
