7f426223fe4f4ba1bf07f08f4a1467ddc6833a6c
Richard Almonte <richard_almonte@hotmail.com>
2022-01-23 17:38:45 +0100

    Add solution to "MiniMax" HackerRank's Problem found at https://www.hackerrank.com/challenges/mini-max-sum/problem

--- ChallengesSolutions.Tests/ProblemsTests.cs
+++ ChallengesSolutions.Tests/ProblemsTests.cs
@@ -45,5 +45,16 @@ namespace ChallengesSolutions
 
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod()]
+        [DataRow(new int[] { 426980153, 354802167, 142980735, 968217435, 734892650 }, 1659655705, 2484892405)]
+        [DataRow(new int[] { 7, 69, 2, 221, 8974 }, 299, 9271)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 10, 14)]
+        public void MiniMaxSum_Test(int[] input, long expectedMin, long expectedMax)
+        {
+            var result = Problems.MiniMaxSum(input);
+            Assert.AreEqual(expectedMin, result[0]);
+            Assert.AreEqual(expectedMax, result[1]);
+        }
     }
 }
--- ChallengesSolutions/Problems.cs
+++ ChallengesSolutions/Problems.cs
@@ -72,5 +72,23 @@
 
             return Math.Abs(diagA - diagB);
         }
+
+
+        /// <summary>
+        /// HackerRank "Mini-Max Sum" Problem
+        /// found at https://www.hackerrank.com/challenges/mini-max-sum/problem
+        /// </summary>
+        /// <param name="arr"></param>
+        public static long[] MiniMaxSum(int[] arr)
+        {
+            /* Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+             * Then print the respective minimum and maximum values as a single line of two space-separated long integers.            
+             */
+            Array.Sort(arr);
+
+            var min = arr.Take(arr.Length - 1).Sum(x => Convert.ToInt64(x));
+            var max = arr.Skip(1).Sum(x => Convert.ToInt64(x));
+            return new long[] { min, max };
+        }
     }
 }
