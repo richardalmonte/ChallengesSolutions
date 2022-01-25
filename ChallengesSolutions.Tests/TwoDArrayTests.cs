3902953ce532e208091d9946460bbfc4911ef78c
Richard Almonte <richard_almonte@hotmail.com>
2022-01-25 21:31:24 +0100

    Add  2D Array Problem Solution

---
+++ ChallengesSolutions.Tests/TwoDArrayTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ChallengesSolutions
+{
+    public partial class ProblemsTests
+    {
+        [TestMethod()]
+        [DataRow(new object[] {
+            new int[] { -9, -9, -9, 1,1,1 },
+            new int[] { 0,-9,0,4,3,2},
+            new int[] { -9,-9,-9,1,2,3 },
+            new int[] {0,0,8,6,6,0 } ,
+            new int[] {0,0,0,-2,0,0 },
+            new int[] {0,0,1,2,4,0 }
+        }, 28)]
+        [DataRow(new object[] {
+            new int[] { 1,1,1,0,0,0 },
+            new int[] { 0,1,0,0,0,0 },
+            new int[] { 1,1,1,0,0,0 },
+            new int[] { 0,0,2,4,4,0} ,
+            new int[] { 0,0,0,2,0,0 },
+            new int[] { 0,0,1,2,4,0 }
+        }, 10)]
+        [DataRow(new object[]
+        {
+            new int[] { -1,-1,0,-9,-2,-2 },
+            new int[] {-2,-1,-6,-8,-2,-5 },
+            new int[] {-1,-1,-1,-2,-3,-4 },
+            new int[] {-1,-9,-2,-4,-4,-5 },
+            new int[] {-7,-3,-3,-2,-9,-9 },
+            new int[] {-1,-3,-1,-2,-4,-5 }
+        }, -6)]
+        public void HourglassSumm_Test(object[] input, int expected)
+        {
+            var result = Problems.HourglassSum(input.Select(x => ((int[])x).ToList()).ToList());
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+    }
+}
---
+++ ChallengesSolutions/TwoDArray.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// HackerRank "2D Array - DS" Problem
+        /// found at https://www.hackerrank.com/challenges/2d-array/problem
+        /// </summary>
+        /// <param name="arr"></param>
+        public static int HourglassSum(List<List<int>> arr)
+        {
+            int max = int.MinValue;
+
+            // On HackerRank site the working solution is using the loop until <= 3 | < 4. Don't know why
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    int sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2]; //First row
+                    sum += arr[row + 1][col + 1]; // Second row. Just one element.
+                    sum += arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2]; //Third row
+
+                    max = Math.Max(max, sum);
+                }
+            }
+            return max;
+        }
+
+    }
+}
