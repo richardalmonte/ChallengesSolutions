fae0760c6ae0b58998001b916e4bc244c1173cd5
Richard Almonte <richard_almonte@hotmail.com>
2022-01-25 19:34:02 +0100

    Add HackerRanks "Gaming Array" problem

--- ChallengesSolutions.Tests/CountingValleysTests.cs
+++ ChallengesSolutions.Tests/CountingValleysTests.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ChallengesSolutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ChallengesSolutions
 {
-
+ 
     public partial class ProblemsTests
     {
         [TestMethod()]
@@ -10,6 +11,8 @@ namespace ChallengesSolutions
         [DataRow(12, "DDUUDDUDUUUD", 2)]
         [DataRow(10, "UDUUUDUDDD", 0)]
         [DataRow(8, "UDDDUDUU", 1)]
+        [DataRow(8, "DDUDUU", 0)]
+        [DataRow(1, "DDDDD", 0)]
         public void CountingValleys_Test(int steps, string path, int expected)
         {
             var result = Problems.CountingValleys(steps, path);
---
+++ ChallengesSolutions/GamingArray.cs
@@ -0,0 +1,48 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// HackerRank "Gaming Array" Problem
+        /// found at https://www.hackerrank.com/challenges/an-interesting-game-1/problem
+        /// </summary>
+        /// <param name="gameSet"></param>
+        public static string GamingArray_Timeout(List<int> gameSet)
+        {
+            var turns = 0;
+            while (gameSet.Count > 0)
+            {
+                var max = gameSet.Max();
+                var maxIndex = gameSet.IndexOf(max);
+                gameSet.RemoveRange(maxIndex, gameSet.Count - maxIndex);
+                turns++;
+            }
+
+            return turns % 2 == 0 ? "ANDY" : "BOB";
+
+        }
+
+        /// <summary>
+        /// HackerRank "Gaming Array" Problem
+        /// found at https://www.hackerrank.com/challenges/an-interesting-game-1/problem
+        /// </summary>
+        /// <param name="arr"></param>
+        public static string GamingArray(List<int> arr)
+        {
+            var previous = 0;
+            var count = 0;
+            for (var i = 0; i < arr.Count; i++)
+            {
+                if (previous < arr[i])
+                {
+                    count++;
+                    previous = arr[i];
+                }
+            }
+            return count % 2 == 0 ? "ANDY" : "BOB";
+
+        }
+
+    }
+}
