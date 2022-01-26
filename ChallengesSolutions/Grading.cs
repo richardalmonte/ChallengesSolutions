28eeefa03428908bdbf2ce2484d5bddd2d1c43a0
Richard Almonte <richard_almonte@hotmail.com>
2022-01-26 01:12:06 +0100

    Add Grading Students Solution - Add TeamInterface with problems from the certification test.

---
+++ ChallengesSolutions/Grading.cs
@@ -0,0 +1,39 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// HackerRank "Grading Students" Problem
+        /// found at https://www.hackerrank.com/challenges/grading/problemproblem
+        /// </summary>
+        /// <param name="arr"></param>
+        public static List<int> GradingStudents(List<int> grades)
+        {
+            // HackerLand University has the following grading policy.
+            // * Every student receives a grade in the inclusive range from 0 to 100.
+            // * any grade less than 40 is a falling grade.
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int grade = grades[i];
+                if (grade < 38)
+                {
+                    continue;
+                }
+
+                int nextMultiple = Convert.ToInt32(Math.Floor((decimal)grade / 5) * 5);
+
+                if (grade % 5 != 0)
+                {
+                    nextMultiple += 5;
+                }
+
+                grades[i] = (nextMultiple - grade < 3) ? nextMultiple : grade;
+            }
+
+            return grades;
+        }
+
+    }
+}
