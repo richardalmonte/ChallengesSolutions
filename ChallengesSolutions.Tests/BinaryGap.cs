ad50f54d0feaa0103bfd210543b3894e893807a2
Richard Almonte <richard_almonte@hotmail.com>
2022-01-26 23:28:41 +0100

    Add solution to Codility's "Binary Gap" problem

---
+++ ChallengesSolutions.Tests/BinaryGap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChallengesSolutions
+{
+
+    public partial class ProblemsTests
+    {
+        [TestMethod()]
+        [DataRow(9, 2)]
+        [DataRow(20, 1)]
+        [DataRow(30, 0)]
+        [DataRow(15, 0)]
+        [DataRow(1041, 5)]
+        public void BinaryGap_Test(int input, int expected)
+        {
+            var actual = Problems.BinaryGap(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ChallengesSolutions/BinaryGap.cs
@@ -0,0 +1,37 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// Codility "Binary Gap" Problem
+        /// found at https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+        /// </summary>
+        /// <param name="arr"></param>
+        public static int BinaryGap(int n)
+        {
+            var binArray = Convert.ToString(n, 2).ToCharArray();
+
+            int lastPosition = 0;
+            int gapSize = 0;
+
+            for (int i = 0; i < binArray.Length; i++)
+            {
+                if (binArray[i] == '0')
+                {
+                    continue;
+                }
+
+                int diff = i - lastPosition;
+                if (diff > gapSize)
+                {
+                    gapSize = diff - 1;
+                }
+                lastPosition = i;
+            }
+
+            return gapSize;
+        }
+
+    }
+}
