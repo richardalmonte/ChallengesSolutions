b0d59c6e0732b4cf62fd27b31c8fe617da2ec92d
Richard Almonte <richard_almonte@hotmail.com>
2022-01-27 10:05:49 +0100

    Add solution to HackerRank "Kangaroo" Problem

---
+++ ChallengesSolutions.Tests/KangarooTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ChallengesSolutions
+{
+    public partial class ProblemsTests
+    {
+        [TestMethod()]
+        [DataRow(2, 1, 1, 2, "YES")]
+        [DataRow(0, 3, 4, 2, "YES")]
+        [DataRow(0, 2, 5, 3, "NO")]
+        public void Kangaroo_Test(int x1, int v1, int x2, int v2, string expected)
+        {
+            var actual = Problems.Kangaroo(x1, v1, x2, v2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ChallengesSolutions/Kangaroo.cs
@@ -0,0 +1,19 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// HackerRank "Number Line Jumps (Kangaroo)" Problem
+        /// found at https://www.hackerrank.com/challenges/kangaroo/problem
+        /// </summary>
+        /// <param name="arr"></param>
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            var x = x2 - x1;
+            var v = v2 - v1;
+            return x * v < 0 && x % v == 0 ? "YES" : "NO";
+        }
+
+    }
+}
