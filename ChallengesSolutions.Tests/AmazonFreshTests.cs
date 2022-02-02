9bebd90ba1bcbba27e3a497e278751e13adbfbcf
Richard Almonte <richard_almonte@hotmail.com>
2022-02-02 17:05:44 +0100

    Add solution to  Amazon Fresh  Promotion problem.
    Both solutions found in the web.

---
+++ ChallengesSolutions.Tests/AmazonFreshTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesSolutions
+{
+    public partial class ProblemsTests
+    {
+        [TestMethod()]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "orange", "apple", "apple", "banana", "orange", "banana" }, true)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "banana", "orange", "banana", "apple", "apple" }, false)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "apple", "banana", "apple", "banana", "orange", "banana" }, false)]
+        [DataRow(new string[] { "apple,apple", "apple,apple,banana" }, new string[] { "apple", "apple", "apple", "banana" }, false)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "orange", "apple", "apple", "banana", "orange", "banana" }, true)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "apple", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }, true)]
+        [DataRow(new string[] { "anything,apple", "banana,anything,banana" }, new string[] { "orange", "grapes", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }, true)]
+        [DataRow(new string[] { "apple,orange", "orange,banana,orange" }, new string[] { "apple", "orange", "banana", "orange", "orange", "banana", "orange", "grape" }, true)]
+        [DataRow(new string[] { "anything,anything,anything,apple", "banana,anything,banana" }, new string[] { "orange", "apple", "banana", "orange", "apple", "orange", "orange", "banana", "apple", "banana" }, true)]
+        public void AmazonFresh_Test(string[] codeListInput, string[] shoppingCart, bool expected)
+        {
+            var actual = Problems.AmazonPromotionWinner(codeListInput.ToList(), shoppingCart.ToList());
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "orange", "apple", "apple", "banana", "orange", "banana" }, true)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "banana", "orange", "banana", "apple", "apple" }, false)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "apple", "banana", "apple", "banana", "orange", "banana" }, false)]
+        [DataRow(new string[] { "apple,apple", "apple,apple,banana" }, new string[] { "apple", "apple", "apple", "banana" }, false)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "orange", "apple", "apple", "banana", "orange", "banana" }, true)]
+        [DataRow(new string[] { "apple,apple", "banana,anything,banana" }, new string[] { "apple", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }, true)]
+        [DataRow(new string[] { "anything,apple", "banana,anything,banana" }, new string[] { "orange", "grapes", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }, true)]
+        [DataRow(new string[] { "apple,orange", "orange,banana,orange" }, new string[] { "apple", "orange", "banana", "orange", "orange", "banana", "orange", "grape" }, true)]
+        [DataRow(new string[] { "anything,anything,anything,apple", "banana,anything,banana" }, new string[] { "orange", "apple", "banana", "orange", "apple", "orange", "orange", "banana", "apple", "banana" }, true)]
+        public void WinPrize_Test(string[] codeListInput, string[] shoppingCart, bool expected)
+        {
+            List<string[]> codeList = new List<string[]>();
+
+            foreach (var code in codeListInput)
+            {
+                codeList.Add(code.Split(',').ToArray());
+            }
+
+            var actual = Problems.WinPrize(codeList.ToArray(), shoppingCart.ToArray());
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ChallengesSolutions/AmazonFresh.cs
@@ -0,0 +1,84 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// Amazon Test "Amazon Fresh Promotion" Problem
+        /// found at https://swabhaw.com/2020/10/07/amazon-fresh-promotion/
+        /// </summary>
+        /// <param name="arr"></param>
+        public static bool AmazonPromotionWinner(List<string> codeList, List<string> shoppingCart)
+        {
+            if (shoppingCart == null || shoppingCart.Count == 0)
+                return false;
+            if (codeList == null || codeList.Count == 0)
+                return true;
+
+            List<List<string>> codeGroups = new List<List<string>>();
+
+            foreach (string code in codeList)
+            {
+                codeGroups.Add(code.Split(',').ToList());
+            }
+
+
+            int groupIndex = 0;
+            int codeIndex = 0;
+            int currCode = 0;
+            for (int i = 0; i < shoppingCart.Count; i++)
+            {
+                if (groupIndex == codeGroups.Count) break;
+                var item = shoppingCart[i];
+                var codeTest = codeGroups[groupIndex][codeIndex];
+                if (codeTest == item || codeTest == "anything")
+                {
+                    codeIndex++;
+                    if (codeIndex == codeGroups[groupIndex].Count)
+                    {
+                        groupIndex++;
+                        codeIndex = 0;
+                    }
+                }
+                else
+                {
+                    codeIndex = 0;
+                }
+
+            }
+            return groupIndex == codeGroups.Count ? true : false;
+        }
+
+        public static bool WinPrize(string[][] codeList, string[] shoppingCart)
+        {
+            // checking corner cases
+            if (codeList == null || codeList.Length == 0)
+                return true;
+            if (shoppingCart == null || shoppingCart.Length == 0)
+                return false;
+
+            int i = 0, j = 0;
+            while (i < codeList.Length && j + codeList[i].Length <= shoppingCart.Length)
+            {
+                bool match = true;
+                for (int k = 0; k < codeList[i].Length; k++)
+                {
+                    if (!codeList[i][k].Equals("anything") && !shoppingCart[j + k].Equals(codeList[i][k]))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    j += codeList[i].Length;
+                    i++;
+                }
+
+                j++;
+
+            }
+            return (i == codeList.Length) ? true : false;
+        }
+    }
+}
