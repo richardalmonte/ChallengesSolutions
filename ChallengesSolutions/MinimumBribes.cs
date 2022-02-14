e3ae7aeb570bd8abcd5c84cf58d2936951dc6b1a
Richard Almonte <richard_almonte@hotmail.com>
2022-02-15 00:50:16 +0100

    Add solution to "New Year Chaos" problem --- incompleted

---
+++ ChallengesSolutions/MinimumBribes.cs
@@ -0,0 +1,32 @@
+﻿namespace ChallengesSolutions
+{
+    public partial class Problems
+    {
+
+        /// <summary>
+        /// HackerRank "New year chaos" Problem
+        /// found at https://www.hackerrank.com/challenges/new-year-chaos/problem
+        /// </summary>
+        /// <param name="arr"></param>
+        public static string MinimumBribes(List<int> arr)
+        {
+            int totalBribes = 0;
+            int bribes = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                var val = arr[i] - 1;
+                if (i >= val)
+                {
+                    continue;
+                }
+
+                bribes = Math.Max(bribes, val - i);
+
+                totalBribes += bribes;
+            }
+
+            return bribes > 2 ? "Too chaotic" : $"{totalBribes}";
+        }
+
+    }
+}
