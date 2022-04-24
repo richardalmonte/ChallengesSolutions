c651280582628f516eb322bb6aa6253bdfb9a8ee
Richard <richardalmonte@users.noreply.github.com>
2022-04-24 19:44:58 +0200

    Solve New Year Chaos HackerRank's problem

--- ChallengesSolutions.Tests/MinimumBribesTests.cs
+++ ChallengesSolutions.Tests/MinimumBribesTests.cs
@@ -9,7 +9,7 @@ namespace ChallengesSolutions
         [DataRow(new int[] { 1, 2, 3, 5, 4, 6, 7, 8 }, "1")]
         [DataRow(new int[] { 4, 1, 2, 3 }, "Too chaotic")]
         [DataRow(new int[] { 2, 1, 5, 3, 4 }, "3")]
-        //[DataRow(new int[] { 1, 2, 5, 3, 7, 8, 6, 4 }, "7")]
+        [DataRow(new int[] { 1, 2, 5, 3, 7, 8, 6, 4 }, "7")]
         public void MinimumBribes_Test(int[] input, string expected)
         {
             var result = Problems.MinimumBribes(input.ToList());
--- ChallengesSolutions/MinimumBribes.cs
+++ ChallengesSolutions/MinimumBribes.cs
@@ -8,24 +8,57 @@
         /// found at https://www.hackerrank.com/challenges/new-year-chaos/problem
         /// </summary>
         /// <param name="arr"></param>
+        //public static string MinimumBribes(List<int> arr)
+        //{
+        //    int totalBribes = 0;
+        //    int bribes = 0;
+        //    for (int i = 0; i < arr.Count; i++)
+        //    {
+        //        var val = arr[i] - 1;
+        //        if (i == val)
+        //        {
+        //            continue;
+        //        }
+
+
+        //        bribes = Math.Max(bribes, val - i);
+
+        //        totalBribes += bribes;
+        //        if (bribes > 2)
+        //        {
+        //            break;
+        //        }
+        //        i += bribes;
+        //    }
+
+        //    return bribes > 2 ? "Too chaotic" : $"{totalBribes}";
+        //}
+
         public static string MinimumBribes(List<int> arr)
         {
-            int totalBribes = 0;
-            int bribes = 0;
-            for (int i = 0; i < arr.Count; i++)
+            int bribe = 0;
+
+            bool chaotic = false;
+            int n = arr.Count;
+
+            for (int i = 0; i < n; i++)
             {
-                var val = arr[i] - 1;
-                if (i >= val)
+                if (arr[i] - (i + 1) > 2)
                 {
-                    continue;
+                    chaotic = true;
+                    break;
                 }
 
-                bribes = Math.Max(bribes, val - i);
-
-                totalBribes += bribes;
+                for (int j = Math.Max(0, arr[i] - 2); j < i; j++)
+                {
+                    if (arr[j] > arr[i])
+                    {
+                        bribe++;
+                    }
+                }
             }
 
-            return bribes > 2 ? "Too chaotic" : $"{totalBribes}";
+            return chaotic ? "Too chaotic" : $"{bribe}";
         }
 
     }
