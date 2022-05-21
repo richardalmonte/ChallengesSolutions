07e73fc14eb8915429e87d0bb21078e802ad0c5e
Richard Almonte <richard_almonte@hotmail.com>
2022-05-21 15:51:29 +0200

    feat(problems): Organize problems into folders by source
    Add Leetcode problem CoinChange. Done with Copilot and my solution.

---
+++ ChallengesSolutions.Tests/LeetCode/CoinChange_Tests.cs
@@ -0,0 +1,30 @@
+﻿using ChallengesSolutions.LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChallengesSolutions.Tests.LeetCode
+{
+    [TestClass]
+    public class CoinChangeTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 5 }, 11, 3)]
+        [DataRow(new int[] { 186, 419, 83, 408 }, 6249, 20)]
+        [DataRow(new int[] { 2 }, 3, -1)]
+        public void CoinChange_Test_Copilot_OK(int[] input, int target, int expected)
+        {
+            var sut = new CoinChangeProblem();
+            var result = sut.CoinChange_Copilot(input, target);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        // [DataRow(new int[] { 1, 2, 5 }, 11, 3)]
+        [DataRow(new int[] { 186, 419, 83, 408 }, 6249, 20)]
+        public void CoinChange_Test_Mine_OK(int[] input, int target, int expected)
+        {
+            var sut = new CoinChangeProblem();
+            var result = sut.CoinChage_Mine(input, target);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ ChallengesSolutions/LeetCode/CoinChangeProblem.cs
@@ -0,0 +1,55 @@
+﻿namespace ChallengesSolutions.LeetCode;
+
+public class CoinChangeProblem
+{
+    /// <summary>
+    /// Solves the coin change problem.
+    /// https://leetcode.com/problems/coin-change/
+    ///  Return the fewest number of coins that you need to make up that amount.
+    ///  If that amount of money cannot be made up by any combination of the coins, return -1.
+    ///  Assume that you have an infinite number of each kind of coin.
+    /// </summary>
+    /// <param name="coins"></param>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public int CoinChange_Copilot(int[] coins, int amount)
+    {
+        if (amount == 0)
+            return 0;
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++)
+        {
+            dp[i] = int.MaxValue;
+            foreach (int coin in coins)
+            {
+                if (i - coin >= 0 && dp[i - coin] != int.MaxValue)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == int.MaxValue ? -1 : dp[amount];
+    }
+
+    public int CoinChage_Mine(int[] coins, int amount)
+    {
+        var sortedCoins = coins;
+        Array.Sort(sortedCoins);
+
+        int result = amount;
+        int counter = 0;
+        for (int i = sortedCoins.Length - 1; i >= 0; i--)
+        {
+            var multiplier = result / sortedCoins[i];
+            counter += multiplier;
+            result = result - (multiplier * sortedCoins[i]);
+        }
+
+
+        return result == 0 ? counter : -1;
+    }
+}
