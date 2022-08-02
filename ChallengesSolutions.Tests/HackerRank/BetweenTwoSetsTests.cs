f82ae44376cb70475bffe05d555481663045d60e
Richard Almonte <richard_almonte@hotmail.com>
2022-08-02 11:17:38 +0200

    feat(problems): Add Solution to HackerRanks "Between two sets" problem
    HackerRanks problem found here:
    https://www.hackerrank.com/challenges/between-two-sets/problem

---
+++ ChallengesSolutions.Tests/HackerRank/BetweenTwoSetsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using ChallengesSolutions.HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChallengesSolutions.Tests.HackerRank;
+
+[TestClass]
+public class BetweenTwoSetsTests
+{
+    private BetweenTwoSets sut;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        sut = new BetweenTwoSets();
+    }
+
+    [TestMethod]
+    [DataRow(new int[] { 2, 4 }, new int[] { 16, 32, 96 }, 3)]
+    [DataRow(new int[] { 3, 4 }, new int[] { 24, 48 }, 2)]
+    public void BetweenTwoSets_Test(int[] inputA, int[] inputB, int expected)
+    {
+        var result = sut.GetTotalX(inputA.ToList(), inputB.ToList());
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ ChallengesSolutions/HackerRank/BetweenTwoSets.cs
@@ -0,0 +1,30 @@
+﻿namespace ChallengesSolutions.HackerRank;
+
+public class BetweenTwoSets
+{
+    public int GetTotalX(List<int> a, List<int> b)
+    {
+        int totalX = 0;
+        int maxA = a.Max();
+        int minB = b.Min();
+        for (var i = maxA; i <= minB; i++)
+        {
+            if (Lcm(a, i) && Gcd(b, i))
+            {
+                totalX++;
+            }
+        }
+
+        return totalX;
+    }
+
+    private static bool Lcm(IEnumerable<int> denoms, int numerator)
+    {
+        return denoms.All(item => numerator % item == 0);
+    }
+
+    private static bool Gcd(IEnumerable<int> numerators, int denom)
+    {
+        return numerators.All(item => item % denom == 0);
+    }
+}
